cc35f27d9c4c054af25081b08dbe6197a8b14566
Ernesto A <ernesto.acosta1086@gmail.com>
2020-05-22 23:26:08 -0400

    List of authors

---
+++ src/app/authors/authors.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthorsService } from '../authors.service';
+
+@Component({
+  selector: 'app-authors',
+  templateUrl: './authors.component.html',
+  styleUrls: ['./authors.component.css']
+})
+export class AuthorsComponent implements OnInit {
+
+  constructor(private aservice: AuthorsService) { }
+
+  authors = this.aservice.getAuthors();
+
+  ngOnInit() {
+  }
+
+}
