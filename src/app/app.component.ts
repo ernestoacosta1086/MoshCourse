a005ab380e6aea3153d7e13c7f862795ffaf7bf4
Ernesto A <ernesto.acosta1086@gmail.com>
2020-05-24 17:31:06 -0400

    LikeCounter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MySolution';
+
+  tweet = {
+    body: '....',
+    isLiked : true,
+    likesCount : 10
+  }
 }
